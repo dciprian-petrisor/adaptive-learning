ec15271e315ea07401313bcaef3fc7f2b3d804af
Petrisor Ciprian <dciprian.petrisor@gmail.com>
2020-12-31 00:49:31 +0200

    fix: moved django secret key  to env file.

--- adaptive_learning/settings.py
+++ adaptive_learning/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7_!0yq8qn+sjlsq66ol(#mc!w0rmm=zbly5hg1riz7^09)@dpf'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'ADAPTIVE_LEARNING_DEBUG' in os.environ
