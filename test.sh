d9f6e203b4ba86994e0b5defd93708cbdb4e8872
Petrisor Ciprian <dciprian.petrisor@gmail.com>
2021-01-01 21:07:22 +0200

    build: added coverage integration with code-climate.

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+set -e
 
-conda run --no-capture-output -n adaptive_learning python manage.py test
+apk install --yes curl # required to download test reporter
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x ./cc-test-reporter # make it executable
+./cc-test-reporter before-build # run before-build hook
+conda run --no-capture-output -n adaptive_learning coverage run --source='.' manage.py test # run the tests
+conda run --no-capture-output -n adaptive_learning coverage xml # output xml
+./cc-test-reporter after-build --exit-code "$TRAVIS_TEST_RESULT" # run after build
