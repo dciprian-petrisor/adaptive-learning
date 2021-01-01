352d5ca9c7b90b8c91114cb3e14dbdd5fab7fb33
Petrisor Ciprian <dciprian.petrisor@gmail.com>
2021-01-01 21:16:02 +0200

    fix: test.sh script uses apt instead of apk

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-apk install --yes curl # required to download test reporter
+apt install --yes curl # required to download test reporter
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 chmod +x ./cc-test-reporter # make it executable
 ./cc-test-reporter before-build # run before-build hook
