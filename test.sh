ad0994ada55af817782510b6250959d5ec27b5d3
Petrisor Ciprian <dciprian.petrisor@gmail.com>
2021-01-01 21:30:15 +0200

    build: code-climage now setting coverage input type.

--- test.sh
+++ test.sh
@@ -2,8 +2,9 @@
 set -e
 
 apt install --yes curl # required to download test reporter
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-chmod +x ./cc-test-reporter # make it executable
-./cc-test-reporter before-build # run before-build hook
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 >./cc-test-reporter
+chmod +x ./cc-test-reporter                                                                 # make it executable
+./cc-test-reporter before-build                                                             # run before-build hook
 conda run --no-capture-output -n adaptive_learning coverage run --source='.' manage.py test # run the tests
-./cc-test-reporter after-build --exit-code "$TRAVIS_TEST_RESULT" # run after build
+conda run --no-capture-output -n adaptive_learning coverage xml                             # output xml
+./cc-test-reporter after-build -t coverage.py --exit-code "$TRAVIS_TEST_RESULT"             # run after build
