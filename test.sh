452e7aa9eedd35f1660f7bab71c44f1548e7dca6
Petrisor Ciprian <dciprian.petrisor@gmail.com>
2021-01-01 22:07:58 +0200

    build: saving exit code to variable.

--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@ set -e
 apt install --yes curl # required to download test reporter
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 >./cc-test-reporter
 chmod +x ./cc-test-reporter                                                                 # make it executable
-./cc-test-reporter before-build                                                             # run before-build hook
+conda run --no-capture-output -n adaptive_learning ./cc-test-reporter before-build          # run before-build hook
 conda run --no-capture-output -n adaptive_learning coverage run --source='.' manage.py test # run the tests
-conda run --no-capture-output -n adaptive_learning coverage xml                             # output xml
-./cc-test-reporter format-coverage -t coverage.py -o ./coverage.xml
-./cc-test-reporter upload-coverage
+test_ecode=$?
+conda run --no-capture-output -n adaptive_learning coverage xml                                             # output xml
+conda run --no-capture-output -n adaptive_learning ./cc-test-reporter after-build --exit-code "$test_ecode" # run after build
