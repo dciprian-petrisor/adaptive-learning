22f0011b70338c77780ba8a81c3e6168975cd669
Petrisor Ciprian <dciprian.petrisor@gmail.com>
2021-01-01 22:30:41 +0200

    build: no longer running cc-test-reporter through conda run

--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@ set -e
 apt install --yes curl # required to download test reporter
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 >./cc-test-reporter
 chmod +x ./cc-test-reporter                                                                 # make it executable
-conda run --no-capture-output -n adaptive_learning ./cc-test-reporter before-build          # run before-build hook
+./cc-test-reporter before-build                                                             # run before-build hook
 conda run --no-capture-output -n adaptive_learning coverage run --source='.' manage.py test # run the tests
-test_ecode=$?
-conda run --no-capture-output -n adaptive_learning coverage xml                                             # output xml
-conda run --no-capture-output -n adaptive_learning ./cc-test-reporter after-build --exit-code "$test_ecode" # run after build
+test_ecode=$?                                                                               # save last command exit code
+conda run --no-capture-output -n adaptive_learning coverage xml                             # output xml
+./cc-test-reporter after-build --exit-code "$test_ecode"                                    # run after build
