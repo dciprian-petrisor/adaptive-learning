57be435866b2d713a626644daf1cf5332d8f1ce0
Petrisor Ciprian <dciprian.petrisor@gmail.com>
2021-01-03 02:56:52 +0200

    build: now passing travis git env vars to test runner container for integration with codeclimate.

--- .travis/test.sh
+++ .travis/test.sh
@@ -11,7 +11,7 @@ IMAGE_TAG="${IMAGE_TAG}-builder"
 # start nginx as well for integration testing
 docker-compose up -d nginx
 # run the tests via docker compose, overriding the image tag so we use the builder image pulled above, which contains the tests.
-docker-compose run --use-aliases -e CC_TEST_REPORTER_ID="${CC_TEST_REPORTER_ID}" -e TRAVIS_TEST_RESULT="${TRAVIS_TEST_RESULT}" --rm web ./test.sh
+docker-compose run --use-aliases -e TRAVIS_PULL_REQUEST_BRANCH -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST_SHA -e TRAVIS_COMMIT -e CC_TEST_REPORTER_ID="${CC_TEST_REPORTER_ID}" --rm web ./test.sh
 # set the image back to what it was
 IMAGE_TAG=${IMAGE_TAG//-builder/}
 # here, all tests passed, so we can tag this image as the new base builder image for subsequent builds for this branch
