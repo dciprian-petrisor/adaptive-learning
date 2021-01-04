656cf26151e7950ed9d0140915713e9d743a4dfa
Petrisor Ciprian <dciprian.petrisor@gmail.com>
2021-01-04 03:34:22 +0200

    build: printing env for debugging secrethub issue in test stage..

--- test.sh
+++ test.sh
@@ -3,11 +3,13 @@
 set -v
 set -x
 
+env
+
 apt install --yes curl # required to download test reporter
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 >./cc-test-reporter
 chmod +x ./cc-test-reporter                                                                           # make it executable
 ./cc-test-reporter before-build                                                                       # run before-build hook
-secrethub --debug run -- conda run --no-capture-output -n adaptive_learning coverage run --source='.' manage.py test --noinput # run the tests
+secrethub run -- conda run --no-capture-output -n adaptive_learning coverage run --source='.' manage.py test --noinput # run the tests
 test_ecode=$?                                                                                         # save last command exit code
-secrethub --debug run -- conda run --no-capture-output -n adaptive_learning coverage xml                                       # output xml
+secrethub run -- conda run --no-capture-output -n adaptive_learning coverage xml                                       # output xml
 ./cc-test-reporter after-build --exit-code "$test_ecode"                                              # run after build
