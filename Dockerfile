94e65273165a105a702ff91ab9957f5fe6a142ff
Petrisor Ciprian <dciprian.petrisor@gmail.com>
2021-01-20 17:48:45 +0200

    build: switched to poetry for creating the build environment.

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,27 @@
-FROM continuumio/miniconda3 AS build
-WORKDIR /conda
+FROM python:3.8.7-slim-buster AS build
+WORKDIR /poetry
 SHELL ["/bin/bash", "-c"]
+RUN apt update --yes
 RUN apt install curl --yes
+RUN apt install libpq-dev --yes
+RUN apt install gcc --yes
+RUN curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
 RUN curl https://apt.secrethub.io | bash
-COPY conda_env.yml /conda/
-RUN conda env create -f conda_env.yml
+COPY poetry.lock /poetry/
+COPY pyproject.toml /poetry/
+RUN ~/.poetry/bin/poetry install
 WORKDIR /app
 COPY . /app/
 RUN chmod +x *.sh
 
-FROM continuumio/miniconda3
+FROM python:3.8.7-slim-buster
 WORKDIR /app
 SHELL ["/bin/bash", "-c"]
+RUN apt update --yes
 RUN apt install curl --yes
 RUN curl https://apt.secrethub.io | bash
-COPY --from=build /opt/conda/envs /opt/conda/envs
+COPY --from=build /root/.poetry /root/.poetry
+COPY --from=build /root/.cache /root/.cache
 COPY --from=build /app /app
 RUN rm -rf /app/tests
 ENV DJANGO_SECRET_KEY=secrethub://petrci1/adaptive_learning_backend/dev/django_secret_key
--- test.sh
+++ test.sh
@@ -5,9 +5,9 @@ set -x
 
 apt install --yes curl # required to download test reporter
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 >./cc-test-reporter
-chmod +x ./cc-test-reporter                                                                           # make it executable
-./cc-test-reporter before-build                                                                       # run before-build hook
-secrethub run -- conda run --no-capture-output -n adaptive_learning coverage run --source='.' manage.py test --noinput # run the tests
-test_ecode=$?                                                                                         # save last command exit code
-secrethub run -- conda run --no-capture-output -n adaptive_learning coverage xml                                       # output xml
-./cc-test-reporter after-build --exit-code "$test_ecode"                                              # run after build
+chmod +x ./cc-test-reporter                                         # make it executable
+./cc-test-reporter before-build                                     # run before-build hook
+secrethub run -- coverage run --source='.' manage.py test --noinput # run the tests
+test_ecode=$?                                                       # save last command exit code
+secrethub run -- coverage xml                                       # output xml
+./cc-test-reporter after-build --exit-code "$test_ecode"            # run after build
