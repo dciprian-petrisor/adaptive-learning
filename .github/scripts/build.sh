94aa33593a3520d23926c1c2c15cae9c2cf4f12d
Petrisor Ciprian <dciprian.petrisor@gmail.com>
2021-04-27 01:17:27 +0300

    chore: buildx cache with compose

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,8 +5,8 @@ set -v
 set -x
 
 # build the nginx image
-docker buildx build --cache-from=type=local,src=/tmp/.buildx-cache --cache-to=type=local,dest=/tmp/.buildx-cache-new -t "${NGINX_IMAGE_NAME}":"${NGINX_IMAGE_TAG}" ./docker/nginx
+docker build -t "${NGINX_IMAGE_NAME}":"${NGINX_IMAGE_TAG}" ./docker/nginx
 # build the postgres image
-docker buildx build --cache-from=type=local,src=/tmp/.buildx-cache --cache-to=type=local,dest=/tmp/.buildx-cache-new -t "${POSTGRES_IMAGE_NAME}":"${POSTGRES_IMAGE_TAG}" ./docker/postgres
+docker build -t "${POSTGRES_IMAGE_NAME}":"${POSTGRES_IMAGE_TAG}" ./docker/postgres
 # build the app, up to the build stage only
-docker buildx build --cache-from=type=local,src=/tmp/.buildx-cache --cache-to=type=local,dest=/tmp/.buildx-cache-new --target build  -t "${IMAGE_NAME}":"${IMAGE_TAG}"-builder .
+docker build --target build  -t "${IMAGE_NAME}":"${IMAGE_TAG}"-builder .
