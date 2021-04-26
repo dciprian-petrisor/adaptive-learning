d524fdb6a99b90f5495dc43a8ec6499518d92499
Petrisor Ciprian <dciprian.petrisor@gmail.com>
2021-04-26 19:15:01 +0300

    fix: updated travis build.sh docker build commands

--- .travis/build.sh
+++ .travis/build.sh
@@ -11,9 +11,9 @@ then
 fi ;
 
 # build the nginx image; tag it as NGINX_IMAGE_TAG
-docker build -t "${NGINX_IMAGE_NAME}":"${NGINX_IMAGE_TAG}" ./nginx
+docker build -t "${NGINX_IMAGE_NAME}":"${NGINX_IMAGE_TAG}" ./docker/nginx
 # build he postgres image; tag it as POSTGRES_IMAGE_TAG
-docker build -t "${POSTGRES_IMAGE_NAME}":"${POSTGRES_IMAGE_TAG}" ./postgres
+docker build -t "${POSTGRES_IMAGE_NAME}":"${POSTGRES_IMAGE_TAG}" ./docker/postgres
 # build only the build stage using the image pulled above as cache; tag it as IMAGE_TAG-builder (which is just git tag + '-builder')
 docker build --target build --cache-from "${IMAGE_NAME}":"${TRAVIS_BRANCH}"-builder -t "${IMAGE_NAME}":"${IMAGE_TAG}"-builder .
 # print all images (for debugging)
