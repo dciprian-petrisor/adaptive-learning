09a71039dae0605552d7b4034eb77920b79ccbe5
Petrisor Ciprian <dciprian.petrisor@gmail.com>
2021-04-27 00:39:17 +0300

    chore: added buildx caching for postgres and nginx

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,8 +5,8 @@ set -v
 set -x
 
 # build the nginx image
-docker buildx build -t "${NGINX_IMAGE_NAME}":"${NGINX_IMAGE_TAG}" ./docker/nginx
+docker buildx build --cache-from=type=local,src=/tmp/.buildx-cache --cache-to=type=local,dest=/tmp/.buildx-cache-new -t "${NGINX_IMAGE_NAME}":"${NGINX_IMAGE_TAG}" ./docker/nginx
 # build the postgres image
-docker buildx build -t "${POSTGRES_IMAGE_NAME}":"${POSTGRES_IMAGE_TAG}" ./docker/postgres
+docker buildx build --cache-from=type=local,src=/tmp/.buildx-cache --cache-to=type=local,dest=/tmp/.buildx-cache-new -t "${POSTGRES_IMAGE_NAME}":"${POSTGRES_IMAGE_TAG}" ./docker/postgres
 # build the app, up to the build stage only
 docker buildx build --cache-from=type=local,src=/tmp/.buildx-cache --cache-to=type=local,dest=/tmp/.buildx-cache-new --target build  -t "${IMAGE_NAME}":"${IMAGE_TAG}"-builder .
